3a3906822a3bdbd42fc5f8e601d4645646f5ab00
Renato Pereira <renatodev95@gmail.com>
2021-04-29 13:33:13 -0300

    OS, SYS, FNMATCH - Convertendo vídeos com Python + FFMPEG

---
+++ secao5-modulos-python/06OS.py
@@ -0,0 +1,46 @@
+# Convertendo videos em Python utilizando a ferramenta ffmpeg
+
+import os
+import fnmatch
+import sys
+
+if sys.platform == 'linux':
+    comando_ffmpeg = 'ffmpeg'
+else:
+    comando_ffmpeg = r'\ffmpeg\ffmpeg.exe'
+
+codec_video = '-c:v libx264'
+crf = '-crf 23'
+preset = '-preset ultrafast'
+codec_audio = '-c:a aac'
+bitrate_audio = '-b:a 320k'
+# Se a variavel debug receber uma string vazia o video será convertido por completo.
+debug = '-ss 00:00:00 -to 00:00:10'
+
+caminho_origem = '/home/user/videos/origin'
+caminho_destino = '/home/user/videos/destiny'
+
+for raiz, pastas, arquivos in os.walk(caminho_origem):
+    for arquivo in arquivos:
+        if not fnmatch.fnmatch(arquivo, '*.mkv'):
+            continue
+        caminho_completo = os.path.join(raiz, arquivo)
+        nome_arquivo, extensao_arquivo = os.path.splitext(caminho_completo)
+        caminho_legenda = nome_arquivo + '.srt'
+
+        if os.path.isfile(caminho_legenda):
+            input_legenda = f'-i "{caminho_legenda}"'
+            map_legenda = '-c:s srt -map v:0 -map a -map 1:0'
+        else:
+            input_legenda = ''
+            map_legenda = ''
+
+        nome_arquivo, extensao_arquivo = os.path.splitext(arquivo)
+
+        arquivo_saida = f'{caminho_destino}/{arquivo}_NOVO{extensao_arquivo}'
+
+        comando = f'{comando_ffmpeg} -i "{caminho_completo}" {input_legenda} ' \
+                  f'{codec_video} {crf} {preset} {codec_audio} {bitrate_audio} '\
+                  f'{debug} {map_legenda} "{arquivo_saida}"'
+
+        os.system(comando)
